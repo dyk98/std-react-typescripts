f391f06dfb2d73adbf3f494ed7f6be39459bbec3
chy <c990330@126.com>
2019-11-12 18:09:12 +0800

    fix:办公室scss

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 // import { Link } from 'react-router-dom';
 import './index.scss';
 import {observer} from 'mobx-react'
-import logo from "../../logo.svg";
 import Office from './Office'
 
 interface IHomeProps {
@@ -23,16 +22,8 @@ class Index extends React.Component<IHomeProps, IHomeState> {
             <div>
 
                 {/*dyk的header组件*/}
-                <div className='header'>
-                    <header className='app_header'>
-                        <img src={logo} className='app_logo' alt="logo"/>
-                        <h1 className="app_title">Welcome to React</h1>
-                    </header>
-                    <p className="app_intro">
-                        To get started, edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                </div>
 
+                {/*办公室*/}
                 <Office/>
 
             </div>
