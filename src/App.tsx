41db5231bbee7afe07473d7a02098ec41f1c751a
dyk666 <124449645@qq.com>
2019-11-12 18:43:48 +0800

    feat:顶部header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.scss';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 import Header from './common/components/Header'
-
+import 'antd/dist/antd.css';
 // 页面
 import Home from './pages/home';
 
--- src/common/components/Header/index.tsx
+++ src/common/components/Header/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 // import { Link } from 'react-router-dom';
 // @ts-ignore
-import styles from './index.scss';
+import styles from "./index.scss";
+import background from '../../../assets/img/header/background.png'
+import { Input } from 'antd';
 import {observer} from 'mobx-react'
 
 interface IHomeProps {
@@ -18,9 +20,21 @@ class Header extends React.Component<IHomeProps, IHomeState> {
     }
 
     public render() {
+        const { Search } = Input;
         return (
             <div className={styles.header}>
-                <p>123</p>
+                <div style={{backgroundImage: `url(${background})`, backgroundSize: '100% 100%'}}
+                     className={styles.header_top}>
+                    <p className={styles.header_top_text}>欢迎您访问儿童安全乘车官网</p>
+                    <Search
+                        placeholder="input search text"
+                        onSearch={value => console.log(value)}
+                        style={{ width: 200 }}
+                    />
+                </div>
+                <div className={styles.header_bottom}>
+                    <img src="" alt="" />
+                </div>
             </div>
         );
     }
